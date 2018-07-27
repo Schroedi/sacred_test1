5945444bca468a392b5867c9cd8ce100381c5d10
Schroedi <call-schroedi@web.de>
2018-07-27 15:07:17 +0200

    Update run.sh
    no sudo for docker

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sudo docker build -t keksdev/pytorch .
+docker build -t keksdev/pytorch .
 
-sudo docker run --runtime=nvidia --ipc=host -it --init --rm --user="$(id -u):$(id -g)" -v ${PWD}:/app keksdev/pytorch bash
+docker run --runtime=nvidia --ipc=host -it --init --rm --user="$(id -u):$(id -g)" -v ${PWD}:/app keksdev/pytorch bash
